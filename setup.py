1099bd56de0f7eb7894a86ee9028300208d84997
FrancoisZiserman <ziserman@gmail.com>
2022-08-11 14:54:10 +0200

    Add __init__.py and setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='dog-training',
+    version='1.0.0',
+    packages=[''],
+    url='',
+    license='',
+    author='François',
+    author_email='ziserman@gmail.com',
+    description='',
+    install_requires=['pytest', "pygame", "sqlalchemy", "pigpio"],
+)
